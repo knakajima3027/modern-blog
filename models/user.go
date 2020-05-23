bc64599addc932ea157f865dd88e2704364c0332
knakajima3027 <kamihate1012@gmail.com>
2020-05-23 18:40:46 +0900

    コードを修正

--- models/user.go
+++ models/user.go
@@ -3,5 +3,5 @@ package models
 type User struct {
     UserId string `gorm:"primary_key" column:"user_id"`
     Name string `gorm:"column:name"`
-	Password string `gorm:"column:password"`
+    Password string `gorm:"column:password"`
 }
