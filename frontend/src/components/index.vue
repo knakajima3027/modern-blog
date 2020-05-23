a2fcc13da8d0e4b04daed2145ff064953dcd4d8a
knakajima3027 <kamihate1012@gmail.com>
2020-05-23 21:08:01 +0900

    update

---
+++ frontend/src/components/index.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="index">
+  <p>記事一覧</p>
+  <ul>
+    <li v-for="post in posts" :key="post.ID">
+      タイトル : <a>{{ post.Title }}</a>
+    <li>
+  </ul>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data () {
+    return {
+      posts: null
+    }
+  },
+  created :function() {
+    this.axios.get('/posts')
+    .then(response => this.posts = response.data)
+  }
+}
+</script>
